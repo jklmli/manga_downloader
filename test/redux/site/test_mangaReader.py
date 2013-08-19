b1a49c2dc5a066843beb6cc7fd888741308513c3
Jiawei Li <jiawei.h.li@gmail.com>
2013-08-18 23:57:51 -0700

    Fix MangaReader test...again.

--- test/redux/site/test_mangaReader.py
+++ test/redux/site/test_mangaReader.py
@@ -3,20 +3,20 @@ from unittest import TestCase
 from redux.site.mangareader import MangaReader
 
 
-class MangaReader(TestCase):
+class TestMangaReader(TestCase):
     SERIES = MangaReader.series('gantz')
     CHAPTERS = SERIES.chapters
 
     def test_chapter_count(self):
-        self.assertEqual(len(MangaReader.SERIES.chapters), 383)
+        self.assertEqual(len(TestMangaReader.SERIES.chapters), 383)
 
     def test_chapter_title(self):
-        self.assertEqual(MangaReader.CHAPTERS[-2].title, 'Lightning Counterstrike')
+        self.assertEqual(TestMangaReader.CHAPTERS[-2].title, 'Lightning Counterstrike')
 
     def test_chapter_pages(self):
-        self.assertEqual(len(MangaReader.CHAPTERS[0].pages), 43)
+        self.assertEqual(len(TestMangaReader.CHAPTERS[0].pages), 43)
 
     def test_for_image_url(self):
-        url = MangaReader.CHAPTERS[0].pages[0].image.url
+        url = TestMangaReader.CHAPTERS[0].pages[0].image.url
         self.assertGreater(len(url), 0)
         self.assertEqual(url[:7], 'http://')
