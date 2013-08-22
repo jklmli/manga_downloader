5ba601709bdf345e57c30c93dc3626b8e3da355c
Jiawei Li <jiawei.h.li@gmail.com>
2013-08-21 22:47:38 -0700

    Rewrite list command using MetaSite.

--- src/manga2.py
+++ src/manga2.py
@@ -1,9 +1,6 @@
 import argparse
-from collections import OrderedDict
 
-from underscore import _
-
-from redux.helper.util import Util
+from metasite import MetaSite
 from redux.site.mangafox import MangaFox
 from redux.site.mangahere import MangaHere
 from redux.site.mangapanda import MangaPanda
@@ -33,23 +30,12 @@ def add_download_subparser(subparsers):
     parser.set_defaults(func=download)
 
 
-def chapters(series):
-    chapters = _.flatten([
-        site.series(series).chapters for site in [MangaFox, MangaHere, MangaPanda, MangaReader]
-    ])
-
-    return OrderedDict(
-        Util.natural_sort(
-            _.groupBy(chapters, lambda chapter, index: chapter.chapter).items(),
-            key=lambda t: t[0]
-        )
-    )
-
-
 def itemize(args):
-    for chapter_number, chapter_objects in chapters(args.series).items():
-        titles = [chapter.title for chapter in chapter_objects if chapter.title.strip() != '']
-        print("(%s) %s" % (chapter_number, titles[0] if len(titles) > 0 else ''))
+    chapters = MetaSite([MangaFox, MangaHere, MangaPanda, MangaReader])\
+        .series(args.series).chapters
+
+    for chapter_number, meta_chapter in chapters.items():
+        print("(%s) %s" % (chapter_number, meta_chapter.title))
 
 
 def download(args):
